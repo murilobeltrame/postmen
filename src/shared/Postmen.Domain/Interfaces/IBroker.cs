a2c8730b52cbfbf0c37aa4da49a01567859be2e7
Murilo Beltrame <murilo.beltrame@ambevtech.com.br>
2022-04-04 10:06:41 -0300

    ....

--- src/shared/Postmen.Domain/Interfaces/IBroker.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Postmen.Domain.Interfaces
-{
-    public interface IBroker
-    {
-        Task Publish<T>(string topicName, T payload);
-
-        Task Listen<T>(string topicName, string subscriptionName, Func<T, Task> handler, Func<Exception, Task>? exceptionHandler);
-    }
-}
