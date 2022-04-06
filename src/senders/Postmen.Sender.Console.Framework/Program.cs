4fca5934b4b4a8fe45bc74435dbbc4bbf43e2c44
Murilo Beltrame <murilo.beltrame@ambevtech.com.br>
2022-04-06 17:49:21 -0300

    ....

--- src/senders/Postmen.Sender.Console.Framework/Program.cs
+++ src/senders/Postmen.Sender.Console.Framework/Program.cs
@@ -1,15 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Postmen.Infrastructure;
+using Postmen.Sender.Application;
+using System;
 using System.Threading.Tasks;
 
 namespace Postmen.Sender.Console.Framework
 {
-    class Program
+    sealed class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            var connectionString = Environment.GetEnvironmentVariable("ConnectionStrings:ServiceBus");
+            var broker = new Broker(connectionString);
+            var service = new ApplicationService(broker);
+
+            while (true)
+            {
+                System.Console.WriteLine("Type your message:");
+                var message = System.Console.ReadLine();
+                await service.PublishAsync(new PostRequest { Description = message }, default);
+                System.Console.WriteLine("Message sent...");
+            }
         }
     }
 }
