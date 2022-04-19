219e857c813494494673e2132d94cdb81cf1972e
Murilo Beltrame <murilo.beltrame@ambevtech.com.br>
2022-04-19 13:50:06 -0300

    ...

---
+++ src/senders/Postmen.Sender.Api.Framework/Broker.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Postmen.Sender.Api.Framework
+{
+    public class Broker: Infrastructure.Broker
+    {
+        public Broker() : base(Environment.GetEnvironmentVariable("ConnectionStrings:ServiceBus")) { }
+    }
+}
